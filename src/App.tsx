a52e01802929797726b279978aee474f5cd33f78
James Whitehead <support@wealthyallianceclub.com>
2025-07-16 16:16:33 -0700

    Add FormationProvider and key routes (formation, login, pricing, services)

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomePage from './pages/HomePage';
+import FormationPage from './pages/FormationPage';
+import LoginPage from './pages/LoginPage';
+import PricingPage from './pages/PricingPage';
+import ServicesPage from './pages/ServicesPage';
+import { FormationProvider } from './context/FormationContext';
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/test" element={<div>Test page works!</div>} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <FormationProvider>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/formation" element={<FormationPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/pricing" element={<PricingPage />} />
+              <Route path="/services" element={<ServicesPage />} />
+              <Route path="/test" element={<div>Test page works!</div>} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </FormationProvider>
     </Router>
   );
 }
