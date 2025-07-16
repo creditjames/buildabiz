0ea6dfe6002b721244ea668f951b114e8f022ae5
James Whitehead <support@wealthyallianceclub.com>
2025-07-16 16:24:34 -0700

    Add HomePage component to test if it causes white screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FormationProvider } from './context/FormationContext';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -10,11 +11,11 @@ function App() {
       <FormationProvider>
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
           <Header />
-          <h1>Test Page - Footer Added!</h1>
-          <p>If you can see this, Footer component is working correctly.</p>
+          <h1>Test Page - HomePage Added!</h1>
+          <p>If you can see this, HomePage component is working correctly.</p>
           <p>Current time: {new Date().toLocaleString()}</p>
           <Routes>
-            <Route path="/" element={<div>Home Route Works!</div>} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/test" element={<div>Test Route Works!</div>} />
           </Routes>
           <Footer />
