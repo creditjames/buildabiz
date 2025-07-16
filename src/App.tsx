409d1ee8ab075d5b33b0ae52867b1983ed492d2d
James Whitehead <support@wealthyallianceclub.com>
2025-07-16 16:25:44 -0700

    Add FormationPage component to test if it causes white screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { FormationProvider } from './context/FormationContext';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import HomePage from './pages/HomePage';
+import FormationPage from './pages/FormationPage';
 
 function App() {
   return (
@@ -11,11 +12,12 @@ function App() {
       <FormationProvider>
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
           <Header />
-          <h1>Test Page - HomePage Added!</h1>
-          <p>If you can see this, HomePage component is working correctly.</p>
+          <h1>Test Page - FormationPage Added!</h1>
+          <p>If you can see this, FormationPage component is working correctly.</p>
           <p>Current time: {new Date().toLocaleString()}</p>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/formation" element={<FormationPage />} />
             <Route path="/test" element={<div>Test Route Works!</div>} />
           </Routes>
           <Footer />
