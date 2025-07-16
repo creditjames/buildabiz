cbf82270bed5d94742dde948f5bbc023d6c2e859
James Whitehead <support@wealthyallianceclub.com>
2025-07-16 16:15:02 -0700

    Add HomePage component to test

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
-
-// Simple test components
-const TestHomePage = () => (
-  <div className="p-8">
-    <h1 className="text-3xl font-bold">Build-A-Biz</h1>
-    <p className="mt-4">Your business formation platform is working!</p>
-  </div>
-);
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
@@ -18,7 +11,7 @@ function App() {
         <Header />
         <main className="flex-grow">
           <Routes>
-            <Route path="/" element={<TestHomePage />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/test" element={<div>Test page works!</div>} />
           </Routes>
         </main>
