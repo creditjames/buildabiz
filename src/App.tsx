0e45bba5c52c39d60279d5096f3b4a5bf014c4f8
James Whitehead <support@wealthyallianceclub.com>
2025-07-16 16:22:36 -0700

    Add Header component to test if it causes white screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FormationProvider } from './context/FormationContext';
+import Header from './components/layout/Header';
 
 function App() {
   return (
     <Router>
       <FormationProvider>
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-          <h1>Test Page - FormationProvider Added!</h1>
-          <p>If you can see this, FormationProvider is working correctly.</p>
+          <Header />
+          <h1>Test Page - Header Added!</h1>
+          <p>If you can see this, Header component is working correctly.</p>
           <p>Current time: {new Date().toLocaleString()}</p>
           <Routes>
             <Route path="/" element={<div>Home Route Works!</div>} />
