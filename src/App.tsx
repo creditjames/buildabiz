f9bdf00aa0838b7af16e6d2707035c3f36d695f5
James Whitehead <support@wealthyallianceclub.com>
2025-07-16 16:23:38 -0700

    Add Footer component to test if it causes white screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FormationProvider } from './context/FormationContext';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
@@ -9,13 +10,14 @@ function App() {
       <FormationProvider>
         <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
           <Header />
-          <h1>Test Page - Header Added!</h1>
-          <p>If you can see this, Header component is working correctly.</p>
+          <h1>Test Page - Footer Added!</h1>
+          <p>If you can see this, Footer component is working correctly.</p>
           <p>Current time: {new Date().toLocaleString()}</p>
           <Routes>
             <Route path="/" element={<div>Home Route Works!</div>} />
             <Route path="/test" element={<div>Test Route Works!</div>} />
           </Routes>
+          <Footer />
         </div>
       </FormationProvider>
     </Router>
